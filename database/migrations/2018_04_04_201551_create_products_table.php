786d91d56625c860ff4eb91792f0125c98d7a3c5
charlytud <charlytud@gmail.com>
2018-04-04 23:36:45 +0200

    altered migrations (varchar to string)

--- database/migrations/2018_04_04_201551_create_products_table.php
+++ database/migrations/2018_04_04_201551_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->varchar('name');
+            $table->string('name');
             $table->text('description');
             $table->double('price');
             $table->integer('stock');
